046c94e227bc2be4f13af9e5a636f1e7914e38ee
Guillaume Bort <g.bort@criteo.com>
2016-02-03 12:32:14 +0100

    Scala training: Option chapter

--- src/test/scala/part1_2/e7_option.scala
+++ src/test/scala/part1_2/e7_option.scala
@@ -3,57 +3,34 @@ package part1_2
 import support.HandsOnSuite
 
 /**
-*   Une Option comme sont nom l’indique représente une valeur optionnelle.
-*   Une instance de Option est soit une instance de Some soit de None
-*   (None dans le cas où il n’y a aucune valeur).
-*
-*   Exemple :
-*   Imaginons que l’on veuille trouver une personne dans une BDD. Elle n’existe peut-être pas.
-*   Dans ce cas en Scala on écrira :
-*
-*       def findPerson(key: Int): Option[Person]
-*
-*   Ca permet de dire que si la personne n’est pas trouvée, alors on renvoie None et non un 'null'
-*   comme dans d’autres langages.
-*   Le type Option permet donc de traiter des cas pour lesquels aucun résultat n’existe,
-*   et de se prémunir des NullPointerException/absence de valeur.
+*  In scala we totally avoid using null (while it still exists for Java compatibility).
+*  Instead of using null we use the Option type.
 *
 *   Quelques liens :
 *     - http://www.scala-lang.org/api/current/index.html#scala.Option
 */
 
 class e7_option extends HandsOnSuite {
-  /**
-  * Quelques tests rapides
-  */
-  exercice("None est égal à...None") {
+
+  exercice("None is...None") {
    None should be(__)
   }
 
-  exercice("None est identique à None") {
-    val a = None
-    // ici on dénote le fait d’être identique, et == l’égalité
-    a should be(__)
-  }
-
-  exercice("None est considéré comme vide") {
+  exercice("None is the empty Option") {
     None.isEmpty should be(__)
-  }
 
-
-  exercice("None peut être utilisé avec le type Option, plutôt qu’avec null") {
     val optional: Option[String] = None
     optional.isEmpty should be(__)
     optional should be(__)
   }
 
-  exercice("Some est le complémentaire de None pour le type Option") {
+  exercice("Some is the filled Option") {
     val optional: Option[String] = Some("Some Value")
     (optional == None) should be(__)
     optional.isEmpty should be(__)
   }
 
-  exercice("Option.getOrElse peut être utilisé pour obtenir une valeur par défaut dans le cas de None") {
+  exercice("Option.getOrElse can be used to extract the Option value, by providing a default value if the Option is empty") {
     val optional: Option[String] = Some("Some Value")
     val optional2: Option[String] = None
     optional.getOrElse("No Value") should be(__)
